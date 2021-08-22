78f78fc00412eac13d75dcf3e9a22ad4a554e2d7
WikiMin3R <60493982+WikiMin3R@users.noreply.github.com>
2021-08-21 21:03:12 -0400

    Update clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -16,9 +16,9 @@
 
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 0
-#define CLIENT_VERSION_MINOR 14
-#define CLIENT_VERSION_REVISION 0
-#define CLIENT_VERSION_BUILD 1
+#define CLIENT_VERSION_MINOR 14         // Updated Aug 21, 2021
+#define CLIENT_VERSION_REVISION 0       // Updated Aug 21, 2021
+#define CLIENT_VERSION_BUILD 1          // Updated Aug 21, 2021
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
