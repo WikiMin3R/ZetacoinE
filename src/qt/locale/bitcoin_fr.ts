20ac29b2d32e41a1b34cd02b6478174248a17f5d
WikiMin3R <60493982+WikiMin3R@users.noreply.github.com>
2022-08-27 12:54:58 -0400

    Updated ZET Address Namespace Info

--- src/qt/locale/bitcoin_fr.ts
+++ src/qt/locale/bitcoin_fr.ts
@@ -1719,8 +1719,8 @@ Cela implique que des frais à hauteur d&apos;au moins %2 seront nécessaires.</
     </message>
     <message>
         <location filename="../sendcoinsdialog.cpp" line="-171"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Entrer une adresse Zetacoin (par ex: EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Entrer une adresse Zetacoin (par ex: 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="+15"/>
@@ -1847,7 +1847,7 @@ Cela implique que des frais à hauteur d&apos;au moins %2 seront nécessaires.</
     </message>
     <message>
         <location line="+34"/>
-        <source>The address to send the payment to (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
+        <source>The address to send the payment to (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1888,8 +1888,8 @@ Cela implique que des frais à hauteur d&apos;au moins %2 seront nécessaires.</
     </message>
     <message>
         <location filename="../sendcoinsentry.cpp" line="+1"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Entrer une adresse Zetacoin (par ex: EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Entrer une adresse Zetacoin (par ex: 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
 </context>
 <context>
@@ -1912,8 +1912,8 @@ Cela implique que des frais à hauteur d&apos;au moins %2 seront nécessaires.</
     </message>
     <message>
         <location line="+18"/>
-        <source>The address to sign the message with (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Entrer une adresse Zetacoin (par ex: EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>The address to sign the message with (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Entrer une adresse Zetacoin (par ex: 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="+10"/>
@@ -1976,8 +1976,8 @@ Cela implique que des frais à hauteur d&apos;au moins %2 seront nécessaires.</
     </message>
     <message>
         <location line="+21"/>
-        <source>The address the message was signed with (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>L&apos;adresse avec laquelle le message à été signé (ex: EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>The address the message was signed with (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>L&apos;adresse avec laquelle le message à été signé (ex: 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="+40"/>
@@ -1992,8 +1992,8 @@ Cela implique que des frais à hauteur d&apos;au moins %2 seront nécessaires.</
     <message>
         <location filename="../signverifymessagedialog.cpp" line="+27"/>
         <location line="+3"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Entrer une adresse Zetacoin (par ex: EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Entrer une adresse Zetacoin (par ex: 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="-2"/>
