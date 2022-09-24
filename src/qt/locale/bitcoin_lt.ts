89326fb4bbec634faa60d2773cc7fd6b30109aab
WikiMin3R <60493982+WikiMin3R@users.noreply.github.com>
2022-09-24 11:36:06 -0400

    Updated Namespace Nomenclature

--- src/qt/locale/bitcoin_lt.ts
+++ src/qt/locale/bitcoin_lt.ts
@@ -16,7 +16,7 @@
         <source>Copyright © 2009-2014 The Bitcoin developers
 Copyright © 2012-2014 The NovaCoin developers
 Copyright © 2014 The BlackCoin developers
-Copyright © 2018-2020 EWMCI, LLC.</source>
+Copyright © 2018-2022 EWMCI, LLC.</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1705,7 +1705,7 @@ Adresas: %4</translation>
     </message>
     <message>
         <location filename="../sendcoinsdialog.cpp" line="-171"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1833,7 +1833,7 @@ Adresas: %4</translation>
     </message>
     <message>
         <location line="+34"/>
-        <source>The address to send the payment to (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
+        <source>The address to send the payment to (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1874,7 +1874,7 @@ Adresas: %4</translation>
     </message>
     <message>
         <location filename="../sendcoinsentry.cpp" line="+1"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
         <translation type="unfinished"/>
     </message>
 </context>
@@ -1898,7 +1898,7 @@ Adresas: %4</translation>
     </message>
     <message>
         <location line="+18"/>
-        <source>The address to sign the message with (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
+        <source>The address to sign the message with (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1962,7 +1962,7 @@ Adresas: %4</translation>
     </message>
     <message>
         <location line="+21"/>
-        <source>The address the message was signed with (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
+        <source>The address the message was signed with (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
         <translation type="unfinished"/>
     </message>
     <message>
@@ -1978,7 +1978,7 @@ Adresas: %4</translation>
     <message>
         <location filename="../signverifymessagedialog.cpp" line="+27"/>
         <location line="+3"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
         <translation type="unfinished"/>
     </message>
     <message>
