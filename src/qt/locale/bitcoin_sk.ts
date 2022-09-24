eb02a26bacda7a0f0f0fd3ebd38f86a63a640ab4
WikiMin3R <60493982+WikiMin3R@users.noreply.github.com>
2022-09-24 11:30:19 -0400

    Updated Namespace Nomenclature

--- src/qt/locale/bitcoin_sk.ts
+++ src/qt/locale/bitcoin_sk.ts
@@ -16,11 +16,11 @@
         <source>Copyright © 2009-2014 The Bitcoin developers
 Copyright © 2012-2014 The NovaCoin developers
 Copyright © 2014 The BlackCoin developers
-Copyright © 2018-2020 EWMCI, LLC.</source>
+Copyright © 2018-2022 EWMCI, LLC.</source>
         <translation>Copyright © 2009-2014 The Bitcoin developers
 Copyright © 2012-2014 The NovaCoin developers
 Copyright © 2014 The BlackCoin developers
-Copyright © 2018-2020 EWMCI, LLC.</translation>
+Copyright © 2018-2022 EWMCI, LLC.</translation>
     </message>
     <message>
         <location line="+15"/>
@@ -1723,8 +1723,8 @@ To znamená, že je potrebný poplatok aspoň %2.</translation>
     </message>
     <message>
         <location filename="../sendcoinsdialog.cpp" line="-171"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Zadajte Zetacoin adresu (napr. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Zadajte Zetacoin adresu (napr. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="+15"/>
@@ -1851,8 +1851,8 @@ To znamená, že je potrebný poplatok aspoň %2.</translation>
     </message>
     <message>
         <location line="+34"/>
-        <source>The address to send the payment to (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Adresa pre poslanie platby (napr. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>The address to send the payment to (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Adresa pre poslanie platby (napr. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="+60"/>
@@ -1892,8 +1892,8 @@ To znamená, že je potrebný poplatok aspoň %2.</translation>
     </message>
     <message>
         <location filename="../sendcoinsentry.cpp" line="+1"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Zadajte Zetacoin adresu (napr. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Zadajte Zetacoin adresu (napr. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
 </context>
 <context>
@@ -1916,8 +1916,8 @@ To znamená, že je potrebný poplatok aspoň %2.</translation>
     </message>
     <message>
         <location line="+18"/>
-        <source>The address to sign the message with (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Adresa na podpísanie správy (napr. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>The address to sign the message with (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Adresa na podpísanie správy (napr. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="+10"/>
@@ -1980,8 +1980,8 @@ To znamená, že je potrebný poplatok aspoň %2.</translation>
     </message>
     <message>
         <location line="+21"/>
-        <source>The address the message was signed with (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Adresa správy bola podpísaná (napr. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>The address the message was signed with (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Adresa správy bola podpísaná (napr. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="+40"/>
@@ -1996,8 +1996,8 @@ To znamená, že je potrebný poplatok aspoň %2.</translation>
     <message>
         <location filename="../signverifymessagedialog.cpp" line="+27"/>
         <location line="+3"/>
-        <source>Enter an Zetacoin address (e.g. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</source>
-        <translation>Zadajte Zetacoin adresu (napr. EWXZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)</translation>
+        <source>Enter an Zetacoin address (e.g. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</source>
+        <translation>Zadajte Zetacoin adresu (napr. 9x0xZeTaCoIn0xIs0xToTaL1Y0xAwEsoMe)</translation>
     </message>
     <message>
         <location line="-2"/>
